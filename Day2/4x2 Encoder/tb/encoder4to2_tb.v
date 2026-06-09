e5af68f92c3937c79d211b2fee15a2e27fb4b230
ammpvt <ammpvt@gmail.com>
2026-06-09 21:46:17 +0530

    Update encoder4to2_tb.v

--- Day2/4x2 Encoder/tb/encoder4to2_tb.v
+++ Day2/4x2 Encoder/tb/encoder4to2_tb.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 09.06.2026 10:48:39
+// Create Date: 09.06.2026 10:42:12
 // Design Name: 
-// Module Name: encoder4to2_tb
+// Module Name: Encoder_4by2_tb
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,26 +20,24 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module encoder4to2_tb();
- reg [3:0]d_tb;
- wire [1:0]b_tb;
- 
-integer m=4'b0000; 
- encoder4to2 dut(d_tb,b_tb);
- 
-initial  
-begin 
-{d_tb}=0;
-end 
-initial 
-begin 
-    for(m=1;m<10;m=m*2)
-    begin
-    
-        #10
-        {d_tb}=m;
-        end
-   $monitor("the value of b_tb is %b ",b_tb);     
-end 
+module Encoder_4by2_tb();
 
+    reg [3:0] d_tb;
+    wire [1:0] b_tb;
+    integer m;
+    Encoder_4by2 dut(d_tb, b_tb);
+    
+    initial
+    begin
+    {d_tb}=0;
+    end
+
+initial
+begin
+for (m=1;m<10;m=m*2)
+begin
+#10;
+{d_tb}=m;
+end
+end
 endmodule
