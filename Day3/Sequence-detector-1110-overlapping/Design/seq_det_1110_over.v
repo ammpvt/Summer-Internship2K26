68aaeda56c04a9ee6e8cfe982b8880c6a43701a4
ammpvt <ammpvt@gmail.com>
2026-06-10 11:42:45 +0530

    Add files via upload

---
+++ Day3/Sequence-detector-1110-overlapping/Design/seq_det_1110_over.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 11:17:50
+// Design Name: 
+// Module Name: seq_det_1110_over
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module seq_det_1110_over(input clk,rst,din ,output reg detected
+
+    );
+    parameter idle=2'b00;
+    parameter s1=2'b01;
+    parameter s2=2'b10;
+    parameter s3=2'b11;
+    reg [1:0] ps,ns;
+    
+    //present state logic
+    always@(posedge clk)
+    begin
+        if (rst)
+        begin
+            ps<=idle;
+        end
+        else 
+        ps<=ns;
+    end
+    
+    //nextstate logic
+    
+        always@(*)
+    begin 
+        case(ps)
+            idle:
+                begin 
+                    detected=0;
+                    if (din==0)
+                        ns=idle;
+                    else
+                        ns=s1;
+                end
+            s1:
+                begin
+                detected<=0;
+                if(din==0)
+                    ns=idle;
+                else
+                    ns=s2;
+                    
+                end
+             s2:
+                begin
+                detected<=0;
+                if(din==0)
+                    ns=idle;
+                else
+                    ns=s3;
+                end    
+             s3:
+                begin 
+                    if(din==0)
+                        begin 
+                             ns=idle;             
+                             detected=1;
+                        
+                        end
+                    else
+                        begin
+                             ns=s3;
+                            detected=0;
+                        end
+                 end
+                
+        endcase
+    end
+    
+    
+endmodule
